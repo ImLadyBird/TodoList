187cd4f7eecb98f7a0269fcb0c9aa011de10d0b1
Negin Momeni <neginmomeni214@gmail.com>
2025-09-09 21:42:10 +0330

    feat : try to add id to each task

--- src/Components/TodoApp.jsx
+++ src/Components/TodoApp.jsx
@@ -24,6 +24,7 @@ export default function TodoApp() {
   async function postTodos({ todo }) {
     try {
       const response = await client.post("/todos/add", {
+        id: Date.now(),
         todo,
         completed: false,
         userId: 1,
